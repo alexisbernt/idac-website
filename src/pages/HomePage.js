f9aabfdd66a501461285052d745b52ba5354bd2b
alexisbernt <129408149+alexisbernt@users.noreply.github.com>
2026-06-24 22:23:57 -0500

    Updates to Home Page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 import FeaturedArticles from "../components/FeaturedArticles";
 import TreasureMap from "../components/TreasureMap";
 import Footer from "../components/Footer";
-// import "../styles/HomePage.css";
+import "../styles/HomePage.css";
 
 function HomePage() {
   return (
@@ -11,17 +11,18 @@ function HomePage() {
 
       <FeaturedArticles />
 
-      <TreasureMap />
+      <hr className="section-divider" />
 
       <section className="home-content">
-        <h2>The Journey Into Identity & Technology</h2>
-
+        <h2>The Journey Into Identity &amp; Technology</h2>
         <p>
           From technology audits and assessments to IAM,
           conferences, podcasts, certifications, and the
           unexpected opportunities that shaped my career.
         </p>
       </section>
+
+      <TreasureMap />
 
       <Footer />
     </>
