e240fb538e70893607456fd4fdf412a75a3c4eea
alexisbernt <129408149+alexisbernt@users.noreply.github.com>
2026-07-02 12:55:12 -0500

    gh pages vs main pages deploy

---
+++ src/pages/BlogPost4.js
@@ -0,0 +1,8 @@
+// Building a "Agent OS"
+
+// I listen to a podcast that is wildly popular among the nerds (like myself) called The AI Daily Brief.
+
+// This isn't an article to promote and talk about the AI Daily Brief, but I did find "AgentOS" through the podcast. Here's a link: https://aidbagentos.ai/projects.
+
+// It took time. Here's some of the takeaways so it takes less of your time.
+
